1d33870feaa2a961f96eb7dd28339aceff9cb0e4
Valentin Hörzi <valentin.hoerzi@gmail.com>
2020-04-24 13:06:32 +0200

    Created @Input() Stuff

--- angularproject/src/app/service-component/service-component.component.ts
+++ angularproject/src/app/service-component/service-component.component.ts
@@ -1,26 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-service-component',
   templateUrl: './service-component.component.html',
-  styleUrls: ['./service-component.component.css']
+  styleUrls: ['./service-component.component.css'],
 })
 export class ServiceComponentComponent implements OnInit {
-
   user: any;
+  @Input('name') userName: string;
 
   constructor() {
-    this.user = {
-      name : 'Foo Bar',
-      title : 'Software Developer',
-      address : '1234 Main St. City, State, 100010',
-      phone: [
 
-      ]
-    };
   }
 
   ngOnInit(): void {
-
+    this.user = {
+      name: this.userName,
+      title: 'Software Developer',
+      address: '1234 Main St. City, State, 100010',
+      phone: [],
+    };
   }
 }
