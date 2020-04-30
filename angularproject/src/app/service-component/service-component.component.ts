21cbde49db4c39a02d9a015b7b525908cb8198c2
swageneder16 <sandra.wageneder@gmail.com>
2020-04-30 11:08:28 +0200

    Snackbar duration

--- angularproject/src/app/service-component/service-component.component.ts
+++ angularproject/src/app/service-component/service-component.component.ts
@@ -26,13 +26,17 @@ export class ServiceComponentComponent implements OnInit {
     public deleteService(service: ServiceRes) {
         let index = this.services.findIndex(s => s === service);
         this.services.splice(index, 1);        
-        this._snackBar.open("Dienst wurde gelöscht!");
+        this._snackBar.open("Dienst wurde gelöscht!","", {
+            duration: 3000
+        });
     }
 
     public editService(service: ServiceRes) {
         let replaceId = this.services.findIndex(s => s.id == service.id);
         this.services[replaceId] = service;
-        this._snackBar.open("Dienst wurde bearbeitet!");
+        this._snackBar.open("Dienst wurde bearbeitet!","", {
+            duration: 3000
+        });
     }
 
     public createNewService() {
@@ -46,7 +50,9 @@ export class ServiceComponentComponent implements OnInit {
         dialogRef.afterClosed().subscribe((service: ServiceDto) => {
             if (!service) return;
             this._service.addService(service).subscribe(serviceRes => this.services.push(serviceRes));
-            this._snackBar.open("Dienst wurde angelegt!");
+            this._snackBar.open("Dienst wurde angelegt!","", {
+                duration: 3000
+            });
         });
     }
 }
